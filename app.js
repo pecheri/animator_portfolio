8c714bc5e5af4c58454447d0e975802e3c8092c7
pecheri <ryo.sr400@gmail.com>
2022-01-16 20:01:00 -0800

    added images and videos

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 // img containers for main-page
-const artwork1 = ["/img/overview_sample.jpeg", "/img/overview_sample2.jpeg","/img/overview_sample3.jpeg","/img/overview_sample4.jpeg","/img/overview_sample5.jpeg"];
+const artwork1 = ["/img/storyboards/dandanmian/1.jpeg","/img/storyboards/dandanmian/2.jpeg","/img/storyboards/dandanmian/3.jpeg","/img/storyboards/dandanmian/4.jpeg","/img/storyboards/dandanmian/5.jpeg","/img/storyboards/dandanmian/6.jpeg"];
 
-const artwork2 = ["/img/overview_sample6.jpeg", "/img/overview_sample7.jpeg","/img/overview_sample8.jpeg","/img/overview_sample9.jpeg","/img/overview_sample10.jpeg"];
+const artwork2 = ["/img/storyboards/spidersdelight/1.jpeg", "/img/storyboards/spidersdelight/2.jpeg","/img/storyboards/spidersdelight/3.jpeg","/img/storyboards/spidersdelight/4.jpeg","/img/storyboards/spidersdelight/5.jpeg","/img/storyboards/spidersdelight/6.jpeg","/img/storyboards/spidersdelight/7.jpeg","/img/storyboards/spidersdelight/8.jpeg","/img/storyboards/spidersdelight/9.jpeg"];
 
-const artwork3 = ["/img/overview_sample11.jpeg", "/img/overview_sample12.jpeg","/img/overview_sample13.jpeg","/img/overview_sample14.jpeg","/img/overview_sample15.jpeg"];
+// const artwork3 = ["/img/overview_sample11.jpeg", "/img/overview_sample12.jpeg","/img/overview_sample13.jpeg","/img/overview_sample14.jpeg","/img/overview_sample15.jpeg"];
+
+const artwork4 = ["/img/storyboards/fidel/1.jpeg", "/img/storyboards/fidel/2.jpeg"];
 
 // img container for charactar-design
-const character = ["img/bikini.PNG", "img/blackgirl.PNG", "img/cat_newyear.PNG", "img/acne.PNG", "img/kapoera.PNG", "img/traveling.PNG"];
+const character = ["img/characters/GoBody_Color.jpg", "img/characters/Kevin_FULLSIZE_v04.png", "img/characters/Killizar.png", "img/characters/Bethora.png", "img/characters/TrooperDuper.png", "img/characters/Buxie.png", "img/characters/WolfCrotch.png", "img/characters/GoobGlob_Color.jpg",  "img/characters/OscarFire.png", "img/characters/ManBo_Color.jpg", "img/characters/KateHatred_Color.jpg", "img/characters/ChestEye.png", "img/characters/SwampAsk.png", "img/characters/WoolyDammage.png", "img/characters/Derpoid.png", "img/characters/Killizar.jpg"];
 
 let imgSrc
 let imgArrow
@@ -59,6 +61,14 @@ $(document).ready(function() {
                 $("#popup-container img").attr("src", artwork3[index - 1]);
             }            
         }
+        if (imgArrow == "artwork4") {
+            let index = artwork4.indexOf(currentImg);
+            if (index - 1 < 0) {
+                $("#popup-container img").attr("src", artwork4[artwork4.length - 1]);
+            } else {
+                $("#popup-container img").attr("src", artwork4[index - 1]);
+            }            
+        }
         let newImg = $("#popup-container img").attr("src");
             ImgPage = $("img[src='" + newImg + "']").attr("alt");
             $("#popup-container p").text(ImgPage);
@@ -92,6 +102,14 @@ $(document).ready(function() {
                 $("#popup-container img").attr("src", artwork3[index + 1]);
             }
         }
+        if (imgArrow == "artwork4") {
+            let index = artwork4.indexOf(currentImg);
+            if (index + 1 > artwork4.length - 1) {
+                $("#popup-container img").attr("src", artwork4[0]);
+            } else {
+                $("#popup-container img").attr("src", artwork4[index + 1]);
+            }
+        }
         let newImg = $("#popup-container img").attr("src");
             ImgPage = $("img[src='" + newImg + "']").attr("alt");
             $("#popup-container p").text(ImgPage);
@@ -107,7 +125,7 @@ $(document).ready(function() {
         $("body,html").css("overflow-y", "hidden");
         return false;
     })
-
+    
     // close video popup
     $("#close-btn-video").click(function() {
         $("#popup-container-video").fadeOut(500);
@@ -137,7 +155,7 @@ $(document).ready(function() {
     })
 
     // left arrow 
-    $(".left-btn-character, #left-character").click(function() {
+    $("#left-character").click(function() {
         let clickImg = $("#popup-container img").attr("src");
         let index = character.indexOf(clickImg);
         if (index - 1 < 0) {
@@ -148,13 +166,13 @@ $(document).ready(function() {
     })
 
     // right arrow 
-    $(".right-btn-character, #right-character").click(function() {
+    $("#right-character").click(function() {
         let clickImg = $("#popup-container img").attr("src");
         let index = character.indexOf(clickImg);
         if (index + 1 > character.length - 1) {
             $("#popup-container img").attr("src", character[0]);
         } else {
             $("#popup-container img").attr("src", character[index + 1]);
-        }        
+        }
     })
 })
