7ba5b5fa9f276b7fc56cf0109690a9ec4cb15ec8
pecheri <ryo.sr400@gmail.com>
2022-01-16 20:07:36 -0800

    fixed images path

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // img containers for main-page
-const artwork1 = ["/img/storyboards/dandanmian/1.jpeg","/img/storyboards/dandanmian/2.jpeg","/img/storyboards/dandanmian/3.jpeg","/img/storyboards/dandanmian/4.jpeg","/img/storyboards/dandanmian/5.jpeg","/img/storyboards/dandanmian/6.jpeg"];
+const artwork1 = ["img/storyboards/dandanmian/1.jpeg","img/storyboards/dandanmian/2.jpeg","img/storyboards/dandanmian/3.jpeg","img/storyboards/dandanmian/4.jpeg","img/storyboards/dandanmian/5.jpeg","img/storyboards/dandanmian/6.jpeg"];
 
-const artwork2 = ["/img/storyboards/spidersdelight/1.jpeg", "/img/storyboards/spidersdelight/2.jpeg","/img/storyboards/spidersdelight/3.jpeg","/img/storyboards/spidersdelight/4.jpeg","/img/storyboards/spidersdelight/5.jpeg","/img/storyboards/spidersdelight/6.jpeg","/img/storyboards/spidersdelight/7.jpeg","/img/storyboards/spidersdelight/8.jpeg","/img/storyboards/spidersdelight/9.jpeg"];
+const artwork2 = ["img/storyboards/spidersdelight/1.jpeg", "img/storyboards/spidersdelight/2.jpeg","img/storyboards/spidersdelight/3.jpeg","img/storyboards/spidersdelight/4.jpeg","img/storyboards/spidersdelight/5.jpeg","img/storyboards/spidersdelight/6.jpeg","img/storyboards/spidersdelight/7.jpeg","img/storyboards/spidersdelight/8.jpeg","img/storyboards/spidersdelight/9.jpeg"];
 
-// const artwork3 = ["/img/overview_sample11.jpeg", "/img/overview_sample12.jpeg","/img/overview_sample13.jpeg","/img/overview_sample14.jpeg","/img/overview_sample15.jpeg"];
+// const artwork3 = ["img/overview_sample11.jpeg", "img/overview_sample12.jpeg","img/overview_sample13.jpeg","img/overview_sample14.jpeg","img/overview_sample15.jpeg"];
 
-const artwork4 = ["/img/storyboards/fidel/1.jpeg", "/img/storyboards/fidel/2.jpeg"];
+const artwork4 = ["img/storyboards/fidel/1.jpeg", "img/storyboards/fidel/2.jpeg"];
 
 // img container for charactar-design
 const character = ["img/characters/GoBody_Color.jpg", "img/characters/Kevin_FULLSIZE_v04.png", "img/characters/Killizar.png", "img/characters/Bethora.png", "img/characters/TrooperDuper.png", "img/characters/Buxie.png", "img/characters/WolfCrotch.png", "img/characters/GoobGlob_Color.jpg",  "img/characters/OscarFire.png", "img/characters/ManBo_Color.jpg", "img/characters/KateHatred_Color.jpg", "img/characters/ChestEye.png", "img/characters/SwampAsk.png", "img/characters/WoolyDammage.png", "img/characters/Derpoid.png", "img/characters/Killizar.jpg"];
